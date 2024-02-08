a93f2cea717c9286219ee3bf8ce1ca05c85cde58
Nitin <nitganwani@gmail.com>
2024-02-08 12:41:57 +0000

    Add edit character functionality

--- src/app/characters/components/card/card.component.ts
+++ src/app/characters/components/card/card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
+import { MatDialog } from '@angular/material/dialog';
+import { EditPageComponent } from '../../pages/edit-page/edit-page.component';
 
 @Component({
   selector: 'characters-character-card',
@@ -9,10 +11,25 @@ import { Character } from '../../interfaces/character.interface';
 export class CardComponent implements OnInit {
   @Input()
   public character!: Character;
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
     if (!this.character) {
       throw new Error('Character is required');
     }
+  }
+
+  openEditDialog(): void {
+    const dialogRef = this.dialog.open(EditPageComponent, {
+      width: '250px',
+      data: { ...this.character }, // Pass the current character data to the dialog
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        // Here you mock the update since you can't actually update the API
+        this.character = { ...result };
+      }
+    });
   }
 }
--- src/app/characters/pages/edit-page/edit-page.component.ts
+++ src/app/characters/pages/edit-page/edit-page.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class EditPageComponent {
+  form: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<EditPageComponent>,
+    @Inject(MAT_DIALOG_DATA) public character: Character
+  ) {
+    this.form = this.fb.group({
+      name: [character.name, Validators.required],
+      location: [character.location.name, Validators.required],
+      status: [character.status, Validators.required],
+    });
+  }
+
+  onSave() {
+    const editedCharacter = {
+      ...this.character,
+      name: this.form.value.name,
+      location: { ...this.character.location, name: this.form.value.location },
+      status: this.form.value.status,
+    };
+    this.dialogRef.close(editedCharacter);
+  }
 }
